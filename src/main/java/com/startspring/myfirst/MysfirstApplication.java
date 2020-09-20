6d154add8b423938bd5b5779bd7f1b138947fbdd
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-09-20 01:18:53 -0300

    Project created

---
+++ src/main/java/com/startspring/myfirst/MysfirstApplication.java
@@ -0,0 +1,13 @@
+package com.startspring.myfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysfirstApplication.class, args);
+	}
+
+}
