3e08bac35f572f3a31d099b056130722c2e8158d
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-09-29 22:19:50 -0300

    Service layer, component registration

---
+++ src/main/java/com/startspring/myfirst/services/UserService.java
@@ -0,0 +1,25 @@
+package com.startspring.myfirst.services;
+
+import com.startspring.myfirst.entities.User;
+import com.startspring.myfirst.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    //método para retornar a lista de usuários.
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
